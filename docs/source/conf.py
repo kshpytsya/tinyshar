7db6e83a08c4b9c720fe9fa17e71bdeb368e97b2
Kyrylo Shpytsya <kshpitsa@gmail.com>
2018-04-25 23:04:44 +0300

    bump version: 0.5.0 → 0.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ copyright = '2018, Kyrylo Shpytsya'
 author = 'Kyrylo Shpytsya'
 
 # The short X.Y version
-version = '0.5.0'
+version = '0.6.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
